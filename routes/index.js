09e621e11cfdf70564c7348273e7a96de358be8b
salehfh <salehfh@code.edu.az>
2022-06-23 20:45:59 +0400

    sehvleri aradan qaldirdim

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,7 @@ var getData = function () {
     var data = {
         'item1': 'https://images.unsplash.com/photo-1563422156298-c778a278f9a5',
         'item2': 'https://images.unsplash.com/photo-1620173834206-c029bf322dba',
-        'item3': 'https://images.unsplash.com/photo-1602491673980-73aa38de027a',
-        'item4': 'https://images.unsplash.com/photo-1620173834206-c029bf322dba'
+        'item3': 'https://images.unsplash.com/photo-1602491673980-73aa38de027a'
     }
     return data;
 }
